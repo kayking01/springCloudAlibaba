b3d48571fec20e79fe089b2a05c27f2d3b5a3c93
wang.kaixuan <wang.kaixuan@ustcinfo.com>
2024-11-19 17:13:54 +0800

    feat(nacos config) @Value 注入配置文件数据动态获取 @RefreshScope

--- product/src/main/java/com/tulingxueyuan/product/controller/ProductController.java
+++ product/src/main/java/com/tulingxueyuan/product/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.tulingxueyuan.product.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 /*** @author wkx * @date 2024年11月18日 16:58 */
 @RestController
 @RequestMapping("/product")
+@RefreshScope
 public class ProductController {
+
+    @Value("${user.name}")
+    private String value;
 
     @RequestMapping(value = "/get/{id}",method = RequestMethod.GET)
     public String getProductById(@PathVariable("id") String id){
         return "获取商品"+id;
+    }
+
+    @RequestMapping(value = "/getValue",method = RequestMethod.GET)
+    public String getValue(){
+        return value;
     }
 }
