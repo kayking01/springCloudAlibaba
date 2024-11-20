e1ac3717fbad45b0e74d51bf5e00ef34e8c4d6e2
wang.kaixuan <wang.kaixuan@ustcinfo.com>
2024-11-20 11:38:40 +0800

    feat(sentinel) sentinel_demo init

---
+++ sentinel_demo/src/test/java/com/tulingxueyuan/sentinel_demo/SentinelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tulingxueyuan.sentinel_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
