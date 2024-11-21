6a52dc3037a525842b9200934214738a03274ff5
wang.kaixuan <wang.kaixuan@ustcinfo.com>
2024-11-21 17:00:17 +0800

    feat(sentinel) alibaba整合Sentinel 实现控制台流控规则配置

--- order/src/main/java/com/tulingxueyuan/order/controller/OrderController.java
+++ order/src/main/java/com/tulingxueyuan/order/controller/OrderController.java
@@ -1,10 +1,14 @@
 package com.tulingxueyuan.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.tulingxueyuan.order.feign.ProductFeignService;
 import com.tulingxueyuan.order.feign.StockFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /***
  * @author wkx
@@ -31,5 +35,33 @@ public class OrderController {
         String msg = stockFeignService.reduce();
         String product = productFeignService.getProductById("1");
         return "Hello Feign!" + msg+product;
+    }
+
+    /**
+     * @Author wkx
+     * @Description //控制台 针对资源 flow 配置 Qps 限流规则
+     **/
+    @RequestMapping("/flowQps")
+    @SentinelResource(value = "flowQps",blockHandler = "flowBlockHandler")
+    public String flowQps(){
+        System.out.println("下单成功");
+
+        return "Hello Sentinel!";
+    }
+
+    /**
+     * @Author wkx
+     * @Description //控制台 针对资源 flow 配置 线程数限流规则，通过不同浏览器同时访问，实现 限流规则
+     **/
+    @RequestMapping("/flowThreadNum")
+    @SentinelResource(value = "flowThreadNum",blockHandler = "flowBlockHandler")
+    public String flowThreadNum() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
+
+        return "Hello Sentinel!";
+    }
+
+    private String flowBlockHandler(BlockException e){
+        return "限流规则";
     }
 }
